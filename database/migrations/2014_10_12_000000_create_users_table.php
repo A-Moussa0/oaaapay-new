5e81c1944a95a0ee37526f67cb11dfb5addedfa4
Abdelrahman-Moussa <abdelrahman.moussa@jumia.com>
2020-09-04 23:31:35 +0200

    added lighthouse-php-auth and updated user model

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,32 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('email', 100)->unique();
             $table->string('password');
+            $table->string('mobile', 45)->unique()->nullable();
+            $table->tinyInteger('membership_id')->default(1);
+            $table->integer('points_balance')->default(0);
+            $table->integer('pending_points_balance')->default(0);
+            $table->integer('total_commissions')->default(0);
+            $table->integer('pending_commissions')->default(0);
+            $table->integer('withdrawn_points')->default(0);
+            $table->dateTime('last_active')->nullable();
+            $table->tinyInteger('suspended')->default(0);
+            $table->smallInteger('total_ads_watched')->default(0);
+            $table->tinyInteger('pioneer')->default(0);
+            $table->tinyInteger('bypass_ip_check')->default(0);
+            $table->tinyInteger('no_referrals')->default(0);
+            $table->integer('redeemed_points')->default(0);
+            $table->smallInteger('offers_bought')->default(0);
+            $table->smallInteger('tasks_completed')->default(0);
+            $table->smallInteger('surveys_completed')->default(0);
+            $table->string('signup_ip', 130);
+            $table->string('username', 255)->nullable();
+            $table->dateTime('registration_date')->nullable();
+            $table->tinyInteger('automatic_withdrawal')->default(0);
+            $table->timestamp('email_verified_at')->nullable();
+            $table->timestamp('mobile_verified_at')->nullable();
+            $table->string('source', 100)->default('Organic');
             $table->rememberToken();
             $table->timestamps();
         });
