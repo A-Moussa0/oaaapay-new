e5b2ed42e97e7a95419b7bf1f7625a99cf159137
Abdelrahman-Moussa <abdelrahman.moussa@jumia.com>
2020-09-09 23:33:30 +0200

    init model w factory & migration

---
+++ database/factories/ExclusivepointMerchantUserFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\ExclusivepointMerchantUser;
+use Faker\Generator as Faker;
+
+$factory->define(ExclusivepointMerchantUser::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
