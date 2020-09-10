c0fb6a9f9831b298c06eb23db72c48f3114741d9
Abdelrahman-Moussa <abdelrahman.moussa@jumia.com>
2020-09-10 12:08:25 +0200

    manager_merchant model init

---
+++ database/factories/ManagerMerchantFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\ManagerMerchant;
+use Faker\Generator as Faker;
+
+$factory->define(ManagerMerchant::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
