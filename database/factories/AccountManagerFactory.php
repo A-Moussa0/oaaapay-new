10043fad4d21e53f0d74019fcb15ac70220178c9
Abdelrahman-Moussa <abdelrahman.moussa@jumia.com>
2020-09-10 12:07:33 +0200

    account_managers model init

---
+++ database/factories/AccountManagerFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\AccountManager;
+use Faker\Generator as Faker;
+
+$factory->define(AccountManager::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
