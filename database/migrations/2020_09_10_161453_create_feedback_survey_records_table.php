7ba6b6afeaf4e6e39993ec2b44f4e2bcb488cebc
Abdelrahman-Moussa <abdelrahman.moussa@jumia.com>
2020-09-26 14:52:50 +0200

    credit_date nullable, rename decline nad open columns

--- database/migrations/2020_09_10_161453_create_feedback_survey_records_table.php
+++ database/migrations/2020_09_10_161453_create_feedback_survey_records_table.php
@@ -19,10 +19,10 @@ class CreateFeedbackSurveyRecordsTable extends Migration
             $table->unsignedBigInteger('feedback_survey_id');
             $table->smallInteger('reward');
             $table->string('credit_status', 45)->nullable();
-            $table->timestamp('credit_date');
+            $table->timestamp('credit_date')->nullable();
             $table->timestamp('answer_date')->nullable();
-            $table->timestamp('open_date')->nullable();
-            $table->timestamp('decline_date')->nullable();
+            $table->timestamp('opened_at')->nullable();
+            $table->timestamp('declined_at')->nullable();
             $table->string('decline_reason', 255)->nullable();
             $table->string('response_id', 255)->nullable();
             $table->timestamps();
