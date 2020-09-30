2d920f17067bbc44e1e6869a8620a4e2e303474d
Abdelrahman-Moussa <abdelrahman.moussa@jumia.com>
2020-09-30 12:27:34 +0200

    do

--- database/factories/MembershipFactory.php
+++ database/factories/MembershipFactory.php
@@ -7,6 +7,6 @@ use Faker\Generator as Faker;
 
 $factory->define(Membership::class, function (Faker $faker) {
     return [
-        //
+        'name'=> Str::random(10),
     ];
 });
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,10 +19,21 @@ use Illuminate\Support\Str;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
+        'referrer_id' => $faker->numberBetween($min = 1, $max = 100),
+        'mobile' => $faker->unique()->e164PhoneNumber,
         'email' => $faker->unique()->safeEmail,
+        'points_balance' => $faker->numberBetween($min = 0, $max = 50000),
+        'pending_points_balance' => $faker->numberBetween($min = 0, $max = 50000),
+        'total_commissions' => $faker->numberBetween($min = 0, $max = 50000),
+        'pending_commissions' => $faker->numberBetween($min = 0, $max = 50000),
+        'withdrawn_points' => $faker->numberBetween($min = 0, $max = 50000),
+        'total_ads_watched' => $faker->numberBetween($min = 0, $max = 1000),
+        'signup_ip' => $faker->ipv4(),
+        'last_active' => now(),
         'email_verified_at' => now(),
+        'mobile_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+        
     ];
 });
