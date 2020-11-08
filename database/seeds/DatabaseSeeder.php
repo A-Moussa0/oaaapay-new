af61a5eae58c316c2b64efa8cb5bf938f1094073
Abdelrahman-Moussa <abdelrahman.moussa@jumia.com>
2020-11-08 09:43:23 +0200

    added more seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,7 +1,15 @@
 <?php
 
 use App\User;
+use App\Offer;
+use App\Category;
+use App\Industry;
+use App\Merchant;
+use App\OfferType;
 use App\Membership;
+use App\AccountManager;
+use App\FeedbackSurvey;
+use App\FeedbackSurveyRecord;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,7 +21,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        factory(Membership::class, 2)->create();
-        factory(User::class, 50)->create();
+        // factory(Membership::class, 2)->create();
+        // factory(User::class, 50)->create();
+        // factory(AccountManager::class, 10)->create();
+        // factory(Industry::class, 10)->create();
+        // factory(Merchant::class, 10)->create();
+        // factory(Category::class, 10)->create();
+        // factory(OfferType::class, 10)->create();
+        // factory(Offer::class, 100)->create();
+        // factory(FeedbackSurvey::class, 10)->create();
+        factory(FeedbackSurveyRecord::class, 10)->create();
     }
 }
